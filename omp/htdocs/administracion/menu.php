3c69a3190f512ea08679d7f3476dc58cf33ad6be
gitsvn <gitsvn@gitsvn.com>
2009-04-24 15:25:08 +0000

    Cambio la distribución en el modulo de administración.

--- omp/htdocs/administracion/menu.php
+++ omp/htdocs/administracion/menu.php
@@ -1,10 +1,15 @@
 <?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
+<head>
     <title>Módulo de Administración </title>
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
-<!--script language=javascript type=text/javascript src=js/popup_calendar.js-->
+	<style>
+	select,input {vertical-align:middle;}
+table td {border-width:0px; border-style:solid; border-color:#0099FF;}
+	table th {border-color:white; border-style:solid; border-width:5px; -moz-border-radius:12px; padding: 5px 0px;}
+	</style>
+	<!--script language=javascript type=text/javascript src=js/popup_calendar.js-->
 </head>
 <?php
 if (isset($_SESSION["s_username"])) {
@@ -30,13 +35,16 @@ $anios = explode  ('~', $anios_calendario);
 								<div class="top"></div>
 								<div class="content">
 <!------------------------------------------------------------------------------------------------->  
-<center>      
-<table border="0" width="90%">
-    <th colspan="2">Altas y Modificaciones del Calendario</th>
-	<tr>
-    <td colspan="2">
-
-	  <form method="POST" name="calendario_edicion" action="calendario.php" target="calendario" onsubmit="window.open('', 'calendario', 'menubar=no,locationbar=no,resizable=yes,top=0,left=0,scrollbars=yes,status=no')">
+<center>
+ 
+<table border="0" width="100%">
+<tr>
+<th width="50%">Calendario</th>
+<th>Esperas Vencidas</th>
+</tr>
+<tr>
+	<td>
+	<form method="POST" name="calendario_edicion" action="calendario.php" target="calendario" onsubmit="window.open('', 'calendario', 'menubar=no,locationbar=no,resizable=yes,top=0,left=0,scrollbars=yes,status=no')" >
 
 	  	<script>
 			function alertselected(){
@@ -53,7 +61,7 @@ $anios = explode  ('~', $anios_calendario);
 			}
 		</script>
 		  
-	    <strong>Año: <select name=anio onChange="alertselected()">    
+	    <strong align="middle">Año: <select name=anio onChange="alertselected()" style="">    
 				<?php
 					$cant = count($anios);
 					for($i=$cant-1;$i>=0;$i--) {
@@ -65,111 +73,106 @@ $anios = explode  ('~', $anios_calendario);
  	  	<span id="nuevo" style="display:none">
  	  	<input id="anio_nuevo" type="text" name="anio_nuevo" size="4">
  	  	</span>
-	  
-      
-      <!--input type="radio" name="opcion" value="EDICION" checked>Editar 
-	  <input type="radio" name="opcion" value="ANIONUEVO">Crear año-->
-  
 	  <input id="submit" type="submit" value="Editar">
-      </form>
-	</td>
-  </tr>
-    <th colspan="2">Cancela Esperas Vencidas</th>
-	<tr>
-    <td colspan="2">
-	
-	  <form method="POST" action="cancela_esperas.php" target="esperas" onsubmit="window.open('', 'esperas', 'menubar=no,locationbar=no,resizable=yes,top=0,left=0,height=420,width=750,scrollbars=yes,status=no')">
-	  <input type="submit" value="Cancela Esperas Vencidas">
-      </form>
-	</td>
-  </tr>
-    <th colspan="2">Altas, Bajas y Modificaciones de Políticas de Circulación</th>
-	<tr>
-    <td colspan="2">
-	
-	  <form method="POST" action="politicas.php" target="politicas" onsubmit="window.open('', 'politicas', 'menubar=no,locationbar=no,resizable=yes,top=0,left=0,height=450,width=750,scrollbars=no,status=no')">
-	  <input type="submit" value="ABM de Políticas">
-      </form>
-	</td>
-  </tr>
-    <th colspan="2">Altas y Modificaciones de Lectores</th>
-  <tr>
-    <td width="70%"><form method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/">
-      <input type="hidden" name="IsisScript" value="administracion/abmlector.xis">
-      <input type="hidden" name="opcion" value="Presentar">
-      <strong>Lector: <input type="text" name="credencial" size="10">
-      <input type="radio" name="id_lector" value="documento" checked>
-      DNI <input type="radio" name="id_lector" value="mfn">
-      MFN&nbsp;&nbsp; <input type="submit" value="Presentar">
-    </form></td>
-    <td width="30%">
-    <form method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/">
-      <input type="hidden" name="IsisScript" value="administracion/abmlector.xis">
-      <input type="hidden" name="opcion" value="Registro Nuevo">
-      <input type="hidden" name="invocado" value="menu.html">
-      <input type="submit" value="Crear Nuevo Registro">
-    </form></strong>
-    </td>
     </form>
-  </tr>
-  
-    <th>Circulación Bibliográfica</th>
-    <th>Otros procedimientos</th>
-
-  <tr>
+    </td>
     <td>
-    	<form name="form_listados" method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/">
-    	 <input type="hidden" name="IsisScript" value="administracion/listados.xis">
-	 <input type="Hidden" name="Orden" value="Prestamo">	
+	    <form method="POST" action="cancela_esperas.php" target="esperas" onsubmit="window.open('', 'esperas', 'menubar=no,locationbar=no,resizable=yes,top=0,left=0,height=420,width=750,scrollbars=yes,status=no')">
+		<input type="submit" value="Cancela Esperas Vencidas">
+		</form>
+    </td>
+</tr>
+<th width="50%">Políticas</th>
+<th>Lectores</th>
+<tr>
+	<td>
+    	<form method="POST" action="politicas.php" target="politicas" onsubmit="window.open('', 'politicas', 'menubar=no,locationbar=no,resizable=yes,top=0,left=0,height=450,width=750,scrollbars=no,status=no')">
+	    <input type="submit" value="ABM de Políticas">
+        </form>
+    </td>
+	<td>
+	    <form method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/" style="display:inline;">
+        <input type="hidden" name="IsisScript" value="administracion/abmlector.xis">
+        <input type="hidden" name="opcion" value="Presentar">
+        <strong>Lector: <input type="text" name="credencial" size="10">
+        <input type="radio" name="id_lector" value="documento" checked>
+        DNI <input type="radio" name="id_lector" value="mfn">
+        MFN<br />
+        <input type="submit" value="Presentar">
+        </form>
+        <form method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/" style="display:inline">
+        <input type="hidden" name="IsisScript" value="administracion/abmlector.xis">
+        <input type="hidden" name="opcion" value="Registro Nuevo">
+        <input type="hidden" name="invocado" value="menu.html">
+        <input type="submit" value="Nuevo">
+        </form></strong>
+        </form> 
+    </td>
+</tr>
+<tr>
+    <th width="50%">Circulación Bibliográfica</th>
+    <th>Otros procedimientos</th>
+</tr>
+<tr>
+	<td align="left">
+        <form name="form_listados" method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/">
+        <input type="hidden" name="IsisScript" value="administracion/listados.xis">
+        <input type="Hidden" name="Orden" value="Prestamo">	
         <input type="radio" value="morosos" name="opcion"><strong>Listado de morosos<br>
         <input type="radio" value="prestamos" name="opcion">Prestamos del día (Estadística)<br>
         <input type="radio" value="id_recibos" name="opcion" checked>Devoluciones del día (Identificación de recibos)<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				Ordenamiento:
-				<select onchange="window.document.form_listados.Orden.value=this.value;" style="font-size : xx-small">
-				<option value="Prestamo" selected>Nro. Papeleta</option>
-				<option value="Devolucion">Cronológico</option>							
-				</select><br>
+        <span style="text-align:center; float:right;">
+        Ordenamiento:
+        <select onchange="window.document.form_listados.Orden.value=this.value;" style="font-size : xx-small">
+        <option value="Prestamo" selected>Nro. Papeleta</option>
+        <option value="Devolucion">Cronológico</option>							
+        </select>
+        </span><br /><br />
         <input type="radio" value="circulante" name="opcion">Prestamos en circulaci&oacute;n<br>				
         <input type="submit" value="Enviar" name="B1"></strong>
         <input type="reset" value="Restablecer" name="B2"> 
-       </form>
+        </form>
     </td>
-    <td width="51%" valign="top"><b><a href="etiquetas.html">Códigos de Barra libros</a><br>
-			<a href="credenciales.html">Generación de credenciales</a><br>
-			<a href="libre_deuda.html">Emisión de Libre Deuda</a></b></td>
-  </tr>
-   <th colspan="2">Base Bibliográfica. 
-   		<i><font size="2">Dispare esta acción cada vez que incorpora o modifica un inventario</font></i>
-   	</th>
+    <td>
+	    <b>
+        <a href="etiquetas.html">Códigos de Barra libros</a><br>
+		<a href="credenciales.html">Generación de credenciales</a><br>
+		<a href="libre_deuda.html">Emisión de Libre Deuda</a>
+        </b>
+    </td>
+</tr>
+</table>
 
-  <tr>
-	<td colspan="2">
-		<form action="/omp/cgi-bin/wxis.exe/omp/administracion/" method="post">
-		<table border="0" style="valign:middle" cellpadding="8px"><tr><td>
-		<input type="submit" value="Actualizar inventarios (& y @)" width="100"></td><td>Si utiliza los símbolos <b><i>&</i></b> y <b><i>@</i></b> para indicar inventarios nuevos y los que se dan de baja
-		<input type="hidden" name="IsisScript" value="administracion/make_InvNuevos.xis">
-		</td>
-		</form>
-		</tr><tr>
-		<td>
-		<form action="/omp/cgi-bin/wxis.exe/omp/administracion/" method="post">
-		<input type="submit" value="         Actualizar inventarios         "></td><td>En caso que no utilize indicadores para inventarios nuevos y dados de baja<br> (el sistema lo detecta automáticamente)
-		<input type="hidden" name="IsisScript" value="administracion/actualizar_bases.xis">
-		</form>
-		</td></tr></table>
-	</td>
-	</tr>
-  	<tr>	
-	<td colspan="2">
-		<form action="/omp/cgi-bin/wxis.exe/omp/administracion/" method="post">
-		<input type="submit" value="Control de consistencia de inventarios">
-		<input type="hidden" name="IsisScript" value="administracion/check_consistencia.xis">
-		</form>
-	</td>
-    <td>	
-    </td>
-  </tr>
+<br /><br />
+<table border="0" width="100%">    
+
+<tr>
+   <th colspan="2">Base Bibliográfica. <i><font size="2">Dispare esta acción cada vez que incorpora o modifica un inventario</font></i></th>
+</tr>
+<tr style="vertical-align:top;">
+<td>
+    <form action="/omp/cgi-bin/wxis.exe/omp/administracion/" method="post">
+    <input type="submit" value="Actualizar inventarios (& y @)">
+    <input type="hidden" name="IsisScript" value="administracion/make_InvNuevos.xis"><br />
+    Si utiliza los símbolos <b><i>&</i></b> y <b><i>@</i></b> para indicar inventarios nuevos y los que se dan de baja
+    </form>
+</td>
+<td>
+    <form action="/omp/cgi-bin/wxis.exe/omp/administracion/" method="post">
+    <input type="submit" value="         Actualizar inventarios         ">
+    <input type="hidden" name="IsisScript" value="administracion/actualizar_bases.xis"><br />
+    En caso que no utilize indicadores para inventarios nuevos y dados de baja<br> (el sistema lo detecta automáticamente)
+    </form>
+</td>
+<tr>
+<td colspan="2">
+    <form action="/omp/cgi-bin/wxis.exe/omp/administracion/" method="post">
+    <input type="submit" value="Control de consistencia de inventarios">
+    <input type="hidden" name="IsisScript" value="administracion/check_consistencia.xis">
+    </form>
+</td>    
+
+</tr>
 </table>
 </center>
 <!------------------------------------------------------------------------------------------------->  
@@ -191,4 +194,6 @@ $anios = explode  ('~', $anios_calendario);
 echo "<META HTTP-EQUIV=Refresh CONTENT=0;URL=login_form.php>";
 }
 ?>
+
 </html>
+
