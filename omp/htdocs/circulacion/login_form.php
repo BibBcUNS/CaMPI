2849241cd52f2028116cbba54c7c1b73e418c7e0
gitsvn <gitsvn@gitsvn.com>
2010-04-29 11:44:06 +0000

    Corrijo estética.

--- omp/htdocs/circulacion/login_form.php
+++ omp/htdocs/circulacion/login_form.php
@@ -1,6 +1,4 @@
 <?php session_start();?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-    "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
     <title>CaMPI - Login </title>
