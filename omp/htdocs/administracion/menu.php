edd90e91bc3651cfed44261c14442237026311b9
rapiriz <rapiriz@gmail.com>
2012-05-24 12:08:47 +0000

    Se agregó la posibilidad de generar los listados de morosos ingresando un intervalo de fecha en la parte de administración del CAMPI.

--- omp/htdocs/administracion/menu.php
+++ omp/htdocs/administracion/menu.php
@@ -36,7 +36,7 @@ table td {border-width:0px; border-style:solid; border-color:#0099FF;}
 	</script>
 
   <body>
-    <div id="head"> 
+  <div id="head"> 
 		<div id="title">CaMPI > Administración (OpenMarcoPolo)</div>
 		<div id="logo"><img src="/omp/images/logocampi.gif" width="120" height="54"></div>
     </div> 
@@ -146,7 +146,155 @@ table td {border-width:0px; border-style:solid; border-color:#0099FF;}
         <form name="form_listados" method="POST" action="/omp/cgi-bin/wxis.exe/omp/administracion/">
         <input type="hidden" name="IsisScript" value="administracion/listados.xis">
         <input type="Hidden" name="Orden" value="Prestamo">	
-        <input type="radio" value="morosos" name="opcion">Listado de morosos<br>
+        <table border="1" bordercolor="#000000" width="100%" cellpadding="2"><tr><td colspan="4">
+            <input type="radio" value="morosos" name="opcion">Listado de morosos<br>
+            </td></tr>
+            <tr><td>
+            Desde: </td><td>
+            Dia:<select name=diad>
+                    
+                    <option value=01 selected>01</option>
+                    <option value=02>02</option>
+                    <option value=03>03</option>
+                    <option value=04>04</option>
+                    <option value=05>05</option>
+                    <option value=06>06</option>
+                    <option value=07>07</option>
+                    <option value=08>08</option>
+                    <option value=09>09</option>
+                    <option value=10>10</option>
+                    <option value=11>11</option>
+                    <option value=12>12</option>
+                    <option value=13>13</option>
+                    <option value=14>14</option>
+                    <option value=15>15</option>
+                    <option value=16>16</option>
+                    <option value=17>17</option>
+                    <option value=18>18</option>
+                    <option value=19>19</option>
+                    <option value=20>20</option>
+                    <option value=21>21</option>
+                    <option value=22>22</option>
+                    <option value=23>23</option>
+                    <option value=24>24</option>
+                    <option value=25>25</option>
+                    <option value=26>26</option>
+                    <option value=27>27</option>
+                    <option value=28>28</option>
+                    <option value=29>29</option>
+                    <option value=30>30</option>
+                    <option value=31>31</option>
+                    </select>
+                    </td><td>
+                    Mes:
+                    <select name=mesd>
+                    <option value=01 selected>01</option>
+                    <option value=02>02</option>
+                    <option value=03>03</option>
+                    <option value=04>04</option>
+                    <option value=05>05</option>
+                    <option value=06>06</option>
+                    <option value=07>07</option>
+                    <option value=08>08</option>
+                    <option value=09>09</option>
+                    <option value=10>10</option>
+                    <option value=11>11</option>
+                    <option value=12>12</option>
+                    </select>
+                    </td>
+                    <td>
+                    Año:
+                    <select name=anod>
+                    <option value=2000>2000</option>
+                    <option value=2001>2001</option>
+                    <option value=2002>2002</option>
+                    <option value=2003>2003</option>
+                    <option value=2004>2004</option>
+                    <option value=2005>2005</option>
+                    <option value=2006>2006</option>
+                    <option value=2007>2007</option>
+                    <option value=2008>2008</option>
+                    <option value=2009>2009</option>
+                    <option value=2010>2010</option>
+                    <option value=2011 selected>2011</option>
+                    <option value=2012>2012</option>
+                    <option value=2013>2013</option>
+                    
+                    </select>
+                    </td></tr>
+                    <tr><td>
+                    Hasta: </td><td>Dia:<select name=diaf>
+                    
+                    <option value=01 selected>01</option>
+                    <option value=02>02</option>
+                    <option value=03>03</option>
+                    <option value=04>04</option>
+                    <option value=05>05</option>
+                    <option value=06>06</option>
+                    <option value=07>07</option>
+                    <option value=08>08</option>
+                    <option value=09>09</option>
+                    <option value=10>10</option>
+                    <option value=11>11</option>
+                    <option value=12>12</option>
+                    <option value=13>13</option>
+                    <option value=14>14</option>
+                    <option value=15>15</option>
+                    <option value=16>16</option>
+                    <option value=17>17</option>
+                    <option value=18>18</option>
+                    <option value=19>19</option>
+                    <option value=20>20</option>
+                    <option value=21>21</option>
+                    <option value=22>22</option>
+                    <option value=23>23</option>
+                    <option value=24>24</option>
+                    <option value=25>25</option>
+                    <option value=26>26</option>
+                    <option value=27>27</option>
+                    <option value=28>28</option>
+                    <option value=29>29</option>
+                    <option value=30>30</option>
+                    <option value=31>31</option>
+                    </select>
+                    </td>
+                    <td>
+                    Mes:
+                    <select name=mesf>
+                    <option value=01 selected>01</option>
+                    <option value=02>02</option>
+                    <option value=03>03</option>
+                    <option value=04>04</option>
+                    <option value=05>05</option>
+                    <option value=06>06</option>
+                    <option value=07>07</option>
+                    <option value=08>08</option>
+                    <option value=09>09</option>
+                    <option value=10>10</option>
+                    <option value=11>11</option>
+                    <option value=12>12</option>
+                    </select>
+                    </td>
+                    <td>
+                    Año:
+                    <select name=anof>
+                    <option value=2000>2000</option>
+                    <option value=2001>2001</option>
+                    <option value=2002>2002</option>
+                    <option value=2003>2003</option>
+                    <option value=2004>2004</option>
+                    <option value=2005>2005</option>
+                    <option value=2006>2006</option>
+                    <option value=2007>2007</option>
+                    <option value=2008>2008</option>
+                    <option value=2009>2009</option>
+                    <option value=2010>2010</option>
+                    <option value=2011>2011</option>
+                    <option value=2012 selected>2012</option>
+                    <option value=2013>2013</option>
+                    
+                    </select>
+        </td></tr></table>
         <input type="radio" value="prestamos" name="opcion">Préstamos del día (Estadística)<br>
         <input type="radio" value="id_recibos" name="opcion" checked>Devoluciones del día<br>
         <center  style="font-size : x-small;">
