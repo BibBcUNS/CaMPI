93866d04a7c284e5db7867d6eb08e35e9b98ae84
MartinAcosta-dev <martinacostadev@gmail.com>
2023-09-27 11:40:07 -0300

    Cambio login y estilos

--- catalogacion/catalis/htdocs/catalis/js/catalis.js
+++ catalogacion/catalis/htdocs/catalis/js/catalis.js
@@ -214,13 +214,13 @@ function setDimensions()
   };
 
   // Búsquedas
-  document.getElementById("searchResultsIframe").style.height = g_Dimensions.searchResultsIframe.height + "px";
+  document.getElementById("searchResultsIframe").style.height = g_Dimensions.searchResultsIframe.height - 25 + "px";
   document.getElementById("recordVisualization").style.height = g_Dimensions.recordVisualization.height + "px";
   document.getElementById("indexTerms").style.height = g_Dimensions.indexTerms.height + "px";
 
   // Edición
-  document.getElementById("theRightPanel").style.height = g_Dimensions.theRightPanel.height + 10 + "px";
-  document.getElementById("recordDiv").style.height = g_Dimensions.recordDiv.height + 10 + "px";
+  document.getElementById("theRightPanel").style.height = g_Dimensions.theRightPanel.height - 6  + "px";
+  document.getElementById("recordDiv").style.height = g_Dimensions.recordDiv.height - 7 + "px";
   document.getElementById("docIframe").style.height = g_Dimensions.docIframeCollapsed.height + "px";
 }
 
